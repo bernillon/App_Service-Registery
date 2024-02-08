ed73c40e555761ffe2c8e3a2f643bf4d7ea350b0
Clement <clement.bernillon@gmail.com>
2024-02-08 10:51:11 +0100

    Inital commit

---
+++ src/main/java/org/aelion/myownserviceregistry/MyownServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package org.aelion.myownserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyownServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyownServiceRegistryApplication.class, args);
+	}
+
+}
