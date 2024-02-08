f85bd48d22ceaf7246441b47d5ddac8f64bcf286
Clement <clement.bernillon@gmail.com>
2024-02-08 14:29:02 +0100

    add configuration Eureka

--- src/main/java/org/aelion/myownserviceregistry/MyownServiceRegistryApplication.java
+++ src/main/java/org/aelion/myownserviceregistry/MyownServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package org.aelion.myownserviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MyownServiceRegistryApplication {
 
 	public static void main(String[] args) {
